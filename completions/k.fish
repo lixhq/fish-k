fda14c370e958d8103ea01bde6abeffae0a865a6
Emil Ingerslev <emil@ingerslev.io>
2017-11-18 20:22:52 +0100

    Extend with _getall and _ namespace options

--- completions/k.fish
+++ completions/k.fish
@@ -35,7 +35,9 @@ set kubectl_resources (kubectl get --show-all 2>&1 | grep '^    \* ' | sed 's/^ 
 
 complete -f -c k -n '__fish_k_needs_command' -a '(kubectl config get-contexts -o name)' -d "Context"
 complete -f -c k -n '__fish_k_arg_number 2' -a '(kubectl --context (set -l cmd (commandline -opc); and echo $cmd[2]) get namespaces -o name | sed 's~namespaces/~~')' -d "Namespace"
-complete -f -c k -n '__fish_k_arg_number 3' -a '$kubectl_actions'
+complete -f -c k -n '__fish_k_arg_number 2' -a '_getall' -d "Get from all namespaces"
+complete -f -c k -n '__fish_k_arg_number 2' -a '_' -d "Skip namespace"
+complete -f -c k -n '__fish_k_arg_number 3;' -a '$kubectl_actions'
 
 complete -f -c k -n '__fish_k_arg_number 4; and __fish_k_using_command get' -a '$kubectl_resources'
 complete -f -c k -n '__fish_k_arg_number 5; and __fish_k_using_command get' -a '(kubectl --context (set -l cmd (commandline -opc); and echo $cmd[2]) --namespace (set -l cmd (commandline -opc); and echo $cmd[3]) get (set -l cmd (commandline -opc); and echo $cmd[5]) -o name | sed "s~.*/~~")'
--- functions/k.fish
+++ functions/k.fish
@@ -1,3 +1,11 @@
 function k -d "kubectl with determinition" -w k
-  kubectl --context $argv[1] --namespace $argv[2] $argv[3..-1]
+  switch $argv[2]
+    case _
+      kubectl --context $argv[1] $argv[3..-1]
+    case _getall
+      kubectl --context $argv[1] get --all-namespaces $argv[3..-1]
+    case '*'
+      kubectl --context $argv[1] --namespace $argv[2] $argv[3..-1]
+  end
+
 end
